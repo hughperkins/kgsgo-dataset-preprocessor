6a622de79367ccb00e090a58de2426f2e14c4c06
Hugh Perkins <hughperkins@gmail.com>
2015-01-11 18:12:51 +0800

    start working on parsing. maybe should use gomill https://github.com/mattheww/gomill.git for parsing?

--- kgs-dataset-preprocessor.py
+++ kgs-dataset-preprocessor.py
@@ -9,6 +9,7 @@ import sys,os,time, os.path
 import urllib
 import zipfile
 import shutil
+import numpy
 
 sKgsUrl = 'http://u-go.net/gamerecords/'
 
@@ -20,6 +21,17 @@ def downloadPage( url ):
 
 def downloadFiles( sTargetDirectory, iMaxFiles ):
     global sKgsUrl
+    # first, ocunt how many we've downlaoded ,to save downlaoding the index page again
+    if iMaxFiles != -1:
+        iCount = 0
+        for sFilename in os.listdir( sTargetDirectory ):
+            sFilepath = sTargetDirectory + '/' + sFilename
+            if os.path.isfile( sFilepath ) and not sFilename.startswith('~'):
+                iCount = iCount + 1
+        if iCount >= iMaxFiles:
+            print 'reached limit of files you requested, skipping other downlaods'
+            return
+    
     page = downloadPage( sKgsUrl )
 #    print page
     splitpage = page.split('<a href="')
@@ -57,12 +69,58 @@ def unzipFiles( sTargetDirectory, iMaxFiles ):
                 print 'reached limit of files you requested, skipping other unzips'
                 return
 
+def walkthroughSgf( datafile, sgfContents ):
+    splitcontents = sgfContents.split('\n')
+    movesString = splitcontents[len(splitcontents)-2]
+#    print movesString
+    splitmovesString = movesString.split(";")
+    for move in splitmovesString:
+        if move.strip() == '':
+            continue
+#        print move
+        playerLetter = move.split('[')[0]
+        moveString = move.split('[')[1]
+        colLetter = moveString[0]
+        rowLetter = moveString[1]
+        print colLetter + "," + rowLetter
+        col = ord(colLetter) - ord('a')
+        if col > 7:
+            col = col - 1
+        print col
+        row = ord(rowLetter) - ord('a')
+        if row > 7:
+            row = row - 1
+        print row
+
+def parseSgfs2( datafile, sDirPath ):
+    for sSgfFilename in os.listdir( sDirPath ):
+        print sSgfFilename
+        sgfile = open( sDirPath + '/' + sSgfFilename, 'r' )
+        contents = sgfile.read()
+        sgfile.close()
+#        print contents
+        if contents.find( 'SZ[19]' ) < 0:
+            print 'not 19x19, skipping'
+            continue
+        walkthroughSgf( datafile, contents )
+        sys.exit(-1)
+
+def parseSgfs( sTargetDirectory, iMaxFiles ):
+    for sDirname in os.listdir( sTargetDirectory ):
+        sDirpath = sTargetDirectory + '/' + sDirname
+        if os.path.isdir( sDirpath ) and not sDirname.startswith('~'):
+            # create a data file for this directory, and read the sgfs into it...
+            datafile = open( sTargetDirectory + '/' + sDirname + '.dat', 'wb' )
+            parseSgfs2( datafile, sDirpath )
+            datafile.close()
+
 def go(sTargetDirectory, iMaxFiles):
     print 'go'
     if not os.path.isdir( sTargetDirectory ):
         os.makedirs( sTargetDirectory )
     downloadFiles( sTargetDirectory, iMaxFiles )
     unzipFiles( sTargetDirectory, iMaxFiles )
+    parseSgfs( sTargetDirectory, iMaxFiles )
 
 if __name__ == '__main__':
     if len(sys.argv) < 2:
